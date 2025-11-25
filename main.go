c217e0c9c94c749644fa0df39ece124db32da7f7
SixtenGedda <sixten.gedda@gmail.com>
2025-11-25 15:47:53 +0100

    Add basic redis functionality

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 	"github.com/sixtengedda/weather-api/config"
 	"io"
 	"log"
 	"net/http"
 )
 
-func checkcache() {
+func checkcache(c *gin.Context, rdb *redis.Client, ctx context.Context) {
+	key := c.Query("location")
+	value, _ := rdb.Get(ctx, key).Result()
+
+	if value != "" {
+		c.JSON(200, gin.H{
+			"location": key,
+			"weather":  value,
+			"cached":   true,
+		})
+		return
+	}
+	c.JSON(404, gin.H{
+		"error": "No weather found for this location",
+	})
 
 }
 
@@ -20,7 +36,7 @@ func callAPI(c *gin.Context) {
 	body, err := io.ReadAll(response.Body)
 	response.Body.Close()
 	if response.StatusCode > 299 {
-		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", response.StatusCode, body)
+		log.Fatalf("Response test failed with status code: %d and\nbody: %s\n", response.StatusCode, body)
 	}
 	if err != nil {
 		log.Fatal(err)
@@ -30,8 +46,19 @@ func callAPI(c *gin.Context) {
 }
 
 func main() {
-	router := gin.Default()
-	router.GET("/weather", callAPI)
 
-	router.Run("localhost:8080")
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "", // no password
+		DB:       0,  // use default DB
+	})
+
+	ctx := context.Background()
+
+	router := gin.Default()
+	router.GET("/weather", func(c *gin.Context) {
+		checkcache(c, rdb, ctx)
+	})
+
+	router.Run(":8080")
 }
